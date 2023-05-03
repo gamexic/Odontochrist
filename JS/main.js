7c23b11b2f3257fde43e031ceba816de840a84ff
Frank Alayza <stephanoherrera4444@gmail.com>
2023-05-03 01:25:25 -0500

    Adding Header

---
+++ JS/main.js
@@ -0,0 +1,43 @@
+/* Scroll Part */
+
+function scrolll() {
+    var left = document.querySelector(".products-bot");
+    left.scrollBy(-250, 0)
+}
+function scrollr() {
+    var right = document.querySelector(".products-bot");
+    right.scrollBy(250, 0)
+}
+
+/* End Scroll Part */
+
+/* Nav Bar Hamburger */
+
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = 'rgb(41, 50, 60)';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+
+/* End Nav Bar Hamburguer */
